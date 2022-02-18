91d7c3841f9e1b881a1beea6f07290416fdc261c
Greg Szabo <greg@philosobear.com>
2022-02-17 22:05:04 -0500

    starkware deversifi cloud init critical fix

--- debian/starkware-deversifi.sh
+++ debian/starkware-deversifi.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 apt install -y xfsprogs lz4
 
 mkfs -t xfs /dev/nvme0n1
-echo "/dev/nvme0n1	/mnt/database	xfs	defaults	0	0" > /etc/fstab
+echo "/dev/nvme0n1	/mnt/database	xfs	defaults	0	0" >> /etc/fstab
 mkdir /mnt/database
 mount /mnt/database
