e115043d94a64dc90544d29c585ff92218dd0ae9
Greg Szabo <greg@philosobear.com>
2022-03-01 14:52:22 -0500

    XFS formatting rules

--- debian/starkware-deversifi.sh
+++ debian/starkware-deversifi.sh
@@ -14,12 +14,12 @@ apt-get install -y xfsprogs lz4
 XFS="new"
 mkfs -t xfs /dev/nvme0n1 || XFS="tainted"
 if [ "$XFS" == "tainted" ]; then
-  log "XFS file system already exists or error formatting. Skipping database setup..."
+  log "XFS file system already exists or error formatting."
 else
-  log "XFS file system formatted. Setting up automount..."
-  echo "/dev/nvme0n1	/mnt/database	xfs	defaults	0	0" >> /etc/fstab
-  mkdir /mnt/database
-  mount /mnt/database
+  log "XFS file system formatted."
 fi
+echo "/dev/nvme0n1	/mnt/database	xfs	defaults	0	0" >> /etc/fstab
+mkdir /mnt/database
+mount /mnt/database
 
 log "Finished custom bootstrap script for $(hostname)"
