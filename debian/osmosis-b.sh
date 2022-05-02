adc775229c35e56704bd2341f77aea8a8c95b55d
Greg Szabo <greg@philosobear.com>
2022-05-02 15:07:11 -0400

    OsmosisB fix

--- debian/osmosis-b.sh
+++ debian/osmosis-b.sh
@@ -14,6 +14,8 @@ KERNEL_RELEASE="$(uname -r)"
 echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports contrib non-free" > /etc/apt/sources.list.d/zfs.list
 apt-get update
 apt-get install -y lz4 "linux-headers-${KERNEL_RELEASE}"
+DEBIAN_FRONTEND=noninteractive apt-get install -y -t "${VERSION_CODENAME}-backports" zfs-dkms --no-install-recommends
+modprobe zfs
 apt-get install -y -t "${VERSION_CODENAME}-backports" zfsutils-linux
 
 ZPOOL="new"
