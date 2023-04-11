c7b2ebe8366d1024f122ff04129fd4013bdd9234
sryps <97310071+sryps@users.noreply.github.com>
2023-04-11 12:29:04 -0400

    hashistack backup script V1

---
+++ backup/bin/hashistack-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+DATE=$(date '+%Y-%-m-%d-%A-%B')
+
+nomad operator snapshot save -address="${NOMAD_SERVER}" /home/hashistack/backups/nomad/nomad-${DATE}.snapshot > /home/hashistack/backups/log/backup-${DATE}.log
+
+consul snapshot save -http-addr="${CONSUL_SERVER}" /home/hashistack/backups/consul/consul-${DATE}.snapshot >> /home/hashistack/backups/log/backup-${DATE}.log
+
+vault operator raft snapshot save -tls-skip-verify -address="${VAULT_SERVER}" /home/hashistack/backups/vault/vault-${DATE}.snapshot >> /home/hashistack/backups/log/backup-${DATE}.log
+
+ls -t /home/hashistack/backups/log/ | tail -n +6 | xargs rm --
+ls -t /home/hashistack/backups/nomad/ | tail -n +6 | xargs rm --
+ls -t /home/hashistack/backups/consul/ | tail -n +6 | xargs rm --
+ls -t /home/hashistack/backups/vault/ | tail -n +6 | xargs rm --
